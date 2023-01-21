28db4a894c8abfa5fb3a82484da3e6918d916a41
Suthichanon Poonkrankay <suthichanon@gmail.com>
2023-01-22 06:18:15 +0700

    like

---
+++ JS/app.js
@@ -0,0 +1,193 @@
+//likebutton1
+const likeBtn1 = document.querySelector(".like__btn1");
+let like_Icon1 = document.querySelector("#like_icon1"),
+  like_count1 = document.querySelector("#like_count1");
+let like_clicked_1 = false;
+likeBtn1.addEventListener("click", () => {
+  if (!like_clicked_1) {
+    like_clicked_1 = true;
+    like_Icon1.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count1.textContent++;
+  } else {
+    like_clicked_1 = false;
+    like_Icon1.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count1.textContent--;
+  }
+});
+//likebutton2
+const likeBtn2 = document.querySelector(".like__btn2");
+let like_Icon2 = document.querySelector("#like_icon2"),
+  like_count2 = document.querySelector("#like_count2");
+let like_clicked_2 = false;
+likeBtn2.addEventListener("click", () => {
+  if (!like_clicked_2) {
+    like_clicked_2 = true;
+    like_Icon2.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count2.textContent++;
+  } else {
+    like_clicked_2 = false;
+    like_Icon2.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count2.textContent--;
+  }
+});
+//likebutton3
+const likeBtn3 = document.querySelector(".like__btn3");
+let like_Icon3 = document.querySelector("#like_icon3"),
+  like_count3 = document.querySelector("#like_count3");
+let like_clicked_3 = false;
+likeBtn3.addEventListener("click", () => {
+  if (!like_clicked_3) {
+    like_clicked_3 = true;
+    like_Icon3.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count3.textContent++;
+  } else {
+    like_clicked_3 = false;
+    like_Icon3.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count3.textContent--;
+  }
+});
+//likebutton4
+const likeBtn4 = document.querySelector(".like__btn4");
+let like_Icon4 = document.querySelector("#like_icon4"),
+  like_count4 = document.querySelector("#like_count4");
+let like_clicked_4 = false;
+likeBtn4.addEventListener("click", () => {
+  if (!like_clicked_4) {
+    like_clicked_4 = true;
+    like_Icon4.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count4.textContent++;
+  } else {
+    like_clicked_4 = false;
+    like_Icon4.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count4.textContent--;
+  }
+});
+//likebutton5
+const likeBtn5 = document.querySelector(".like__btn5");
+let like_Icon5 = document.querySelector("#like_icon5"),
+  like_count5 = document.querySelector("#like_count5");
+let like_clicked_5 = false;
+likeBtn5.addEventListener("click", () => {
+  if (!like_clicked_5) {
+    like_clicked_5 = true;
+    like_Icon5.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count5.textContent++;
+  } else {
+    like_clicked_5 = false;
+    like_Icon5.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count5.textContent--;
+  }
+});
+//likebutton6
+const likeBtn6 = document.querySelector(".like__btn6");
+let like_Icon6 = document.querySelector("#like_icon6"),
+  like_count6 = document.querySelector("#like_count6");
+let like_clicked_6 = false;
+likeBtn6.addEventListener("click", () => {
+  if (!like_clicked_6) {
+    like_clicked_6 = true;
+    like_Icon6.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    like_count6.textContent++;
+  } else {
+    like_clicked_6 = false;
+    like_Icon6.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    like_count6.textContent--;
+  }
+});
+//sadbutton1
+const sadBtn1 = document.querySelector(".sad__btn1");
+let sad_Icon1 = document.querySelector("#sad_icon1"),
+  sad_count1 = document.querySelector("#sad_count1");
+let sad_clicked_1 = false;
+sadBtn1.addEventListener("click", () => {
+  if (!sad_clicked_1) {
+    sad_clicked_1 = true;
+    sad_Icon1.innerHTML = `<i class="fa-sharp fa-solid fa-face-sad-tear"></i>`;
+    sad_count1.textContent++;
+  } else {
+    sad_clicked_1 = false;
+    sad_Icon1.innerHTML = `<i class="fa-sharp fa-solid fa-face-sad-tear"></i>`;
+    sad_count1.textContent--;
+  }
+});
+//sadbutton2
+const sadBtn2 = document.querySelector(".sad__btn2");
+let sad_Icon2 = document.querySelector("#sad_icon2"),
+  sad_count2 = document.querySelector("#sad_count2");
+let sad_clicked_2 = false;
+sadBtn2.addEventListener("click", () => {
+  if (!sad_clicked_2) {
+    sad_clicked_2 = true;
+    sad_Icon2.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count2.textContent++;
+  } else {
+    sad_clicked_2 = false;
+    sad_Icon2.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count2.textContent--;
+  }
+});
+//sadbutton3
+const sadBtn3 = document.querySelector(".sad__btn3");
+let sad_Icon3 = document.querySelector("#sad_icon3"),
+  sad_count3 = document.querySelector("#sad_count3");
+let sad_clicked_3 = false;
+sadBtn3.addEventListener("click", () => {
+  if (!sad_clicked_3) {
+    sad_clicked_3 = true;
+    sad_Icon3.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count3.textContent++;
+  } else {
+    sad_clicked_3 = false;
+    sad_Icon3.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count3.textContent--;
+  }
+});
+//sadbutton4
+const sadBtn4 = document.querySelector(".sad__btn4");
+let sad_Icon4 = document.querySelector("#sad_icon4"),
+  sad_count4 = document.querySelector("#sad_count4");
+let sad_clicked_4 = false;
+sadBtn4.addEventListener("click", () => {
+  if (!sad_clicked_4) {
+    sad_clicked_4 = true;
+    sad_Icon4.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count4.textContent++;
+  } else {
+    sad_clicked_4 = false;
+    sad_Icon4.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count4.textContent--;
+  }
+});
+//sadbutton5
+const sadBtn5 = document.querySelector(".sad__btn5");
+let sad_Icon5 = document.querySelector("#sad_icon5"),
+  sad_count5 = document.querySelector("#sad_count5");
+let sad_clicked_5 = false;
+sadBtn5.addEventListener("click", () => {
+  if (!sad_clicked_5) {
+    sad_clicked_5 = true;
+    sad_Icon5.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count5.textContent++;
+  } else {
+    sad_clicked_5 = false;
+    sad_Icon5.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count5.textContent--;
+  }
+});
+//sadbutton6
+const sadBtn6 = document.querySelector(".sad__btn6");
+let sad_Icon6 = document.querySelector("#sad_icon6"),
+  sad_count6 = document.querySelector("#sad_count6");
+let sad_clicked_6 = false;
+sadBtn6.addEventListener("click", () => {
+  if (!sad_clicked_6) {
+    sad_clicked_6 = true;
+    sad_Icon6.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count6.textContent++;
+  } else {
+    sad_clicked_6 = false;
+    sad_Icon6.innerHTML = `<i class="fa-solid fa-face-sad-tear">`;
+    sad_count6.textContent--;
+  }
+});
+
